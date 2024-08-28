d31e1301d1ac7dd72dd69d2ed320fc95c6d6062b
Gunapriya23 <159014634+Gunapriya23@users.noreply.github.com>
2024-08-28 19:55:14 +0530

    Add files via upload

---
+++ led.ino
@@ -0,0 +1,14 @@
+int led = 12;
+void setup() {
+  pinMode(led,OUTPUT);
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  digitalWrite(led,HIGH);
+  delay(1000);
+  digitalWrite(led,LOW);
+  delay(1000);
+  // put your main code here, to run repeatedly:
+
+}
